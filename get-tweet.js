592ab9a664f6a79ec7e89d8d6ff2873f0e220479
Jimmy King <jimmy.king@meltmedia.com>
2013-06-24 21:00:45 -0700

    Added option to return an array of 2-20 tweets, fixes #4

--- get-tweet.js
+++ get-tweet.js
@@ -6,11 +6,11 @@
  */
 
 var getTweet = function () {
-  var tweet = {},
+  var numTweets,
       callbackFn;
 
   return {
-    please: function(id, callback) {
+    please: function(id, callback, howMany) {
       var src = '//cdn.syndication.twimg.com/widgets/timelines/' + id + '?&lang=en&callback=getTweet.process&suppress_response_codes=true&rnd=' + Math.random(),
           fakeAPI = '<script src="' + src + '"></script>';
 
@@ -19,33 +19,57 @@ var getTweet = function () {
       }
 
       callbackFn = callback;
+
+      if (howMany) {
+        numTweets = howMany;
+      }
     },
-    
+
     process: function(data) {
-      var $fakeAPI = $('<div class="fake-api"></div>');
+      var $fakeAPI = $('<div class="fake-api"></div>')
+          tweets = [];
 
       $fakeAPI.append(data.body);
 
-      var $latestTweet = $fakeAPI.children('.root').children('.stream').children('.h-feed').children('.h-entry[data-tweet-id]').eq(0),
-          $tweetContent = $latestTweet.children('.e-entry-content').children('.e-entry-title'),
-          $userInfo = $latestTweet.children('.h-card').children('.profile');
-
-      tweet = {
-        html: $tweetContent.html(),
-        time: Date.parse($latestTweet.children('.permalink').data('datetime')),
-        link: $latestTweet.children('.permalink').attr('href'),
-        user: $userInfo.children('.p-nickname').text(),
-        text: $tweetContent.text(),
-        name: $userInfo.children('.full-name').text().trim(),
-        pic: $userInfo.children('img').attr('src')
+      if ((typeof numTweets != 'number') ||
+         (numTweets < 2) || (numTweets > 20)) {
+        numTweets = 1;
       }
 
-      hereYouGo (tweet, callbackFn);
+      for (i = 0; i < numTweets; i++) {
+        var $thisTweet = $fakeAPI.children('.root').children('.stream').children('.h-feed').children('.h-entry[data-tweet-id]').eq(i),
+            $tweetContent = $thisTweet.children('.e-entry-content').children('.e-entry-title'),
+            $userInfo = $thisTweet.children('.h-card').children('.profile');
+
+        thisTweet = {
+          html: $tweetContent.html(),
+          link: $thisTweet.children('.permalink').attr('href'),
+          name: $userInfo.children('.full-name').text().trim(),
+          pic: $userInfo.children('img').attr('src'),
+          text: $tweetContent.text(),
+          time: Date.parse($thisTweet.children('.permalink').data('datetime')),
+          user: $userInfo.children('.p-nickname').text()
+        }
+
+        tweets.push(thisTweet);
+      }
+
+      if (tweets.length > 1) {
+        hereYouGo(tweets, callbackFn);
+      } else {
+        var tweet = tweets[0];
+        hereYouGo(tweet, callbackFn);
+      }
     }
   }
   
-  function hereYouGo (obj, callback) {
-    getTweet.tweet = obj;
+  function hereYouGo (out, callback) {
+    if (Array.isArray(out)) {
+      getTweet.tweets = out,
+      getTweet.tweet  = out[0];
+    } else {
+      getTweet.tweet  = out;
+    }
     callback();
   }
 }();
