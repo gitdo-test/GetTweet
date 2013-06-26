39125e75cb6b58431fbce06b681ce94fddb25398
Jimmy King <jimmy.king@meltmedia.com>
2013-06-25 19:47:16 -0700

    Added ability to pull from more than one widget, fixes #5 & #1

--- get-tweet.js
+++ get-tweet.js
@@ -1,42 +1,52 @@
 /*
- * get-tweet.js v0.2 by Jimmy King
+ * get-tweet.js v0.2.1 by Jimmy King
  * Licensed under The MIT License
  * Issues and feature requests at:
  * https://github.com/jking90/get-tweet
  */
 
 var getTweet = function () {
-  var numTweets,
-      callbackFn;
 
   return {
-    please: function(id, callback, howMany) {
-      var src = '//cdn.syndication.twimg.com/widgets/timelines/' + id + '?&lang=en&callback=getTweet.process&suppress_response_codes=true&rnd=',
-          fakeAPI = '<script src="' + src + Math.random() + '"></script>';
-
-      if ($('script[src^="' + src + '"]').length === 0) {
-        $('head').eq(0).append(fakeAPI);
+    please: function(widget, callbackFn, howMany, name) {
+      if (typeof howMany === 'string') {
+        name = howMany;
+      }
+      
+      if (typeof name != 'string') {
+        name = 'gtDefault';
       }
 
-      callbackFn = callback;
+      if ((typeof howMany != 'number') ||
+         (howMany < 2) || (howMany > 20)) {
+        howMany = 1;
+      }
 
-      if (howMany) {
-        numTweets = howMany;
+      getTweet[name] = {
+        id: widget,
+        callback: callbackFn,
+        numTweets: howMany
+      }
+
+      getTweet.process[name] = function(data) {
+        getTweet.process(data, name);
+      }
+
+      var src = '//cdn.syndication.twimg.com/widgets/timelines/' + getTweet[name].id + '?&lang=en&callback=getTweet.process.' + name + '&suppress_response_codes=true&rnd=',
+          script = '<script src="' + src + Math.random() + '"></script>';
+
+      if ($('script[src^="' + src + '"]').length === 0) {
+        $('head').eq(0).append(script);
       }
     },
 
-    process: function(data) {
-      var $fakeAPI = $('<div class="fake-api"></div>')
+    process: function(data, name) {
+      var $fakeAPI = $('<div class="fake-api"></div>').append(data.body),
           tweets = [];
+          
+      console.log(this);
 
-      $fakeAPI.append(data.body);
-
-      if ((typeof numTweets != 'number') ||
-         (numTweets < 2) || (numTweets > 20)) {
-        numTweets = 1;
-      }
-
-      for (i = 0; i < numTweets; i++) {
+      for (i = 0; i < getTweet[name].numTweets; i++) {
         var $thisTweet = $fakeAPI.children('.root').children('.stream').children('.h-feed').children('.h-entry[data-tweet-id]').eq(i),
             $tweetContent = $thisTweet.children('.e-entry-content').children('.e-entry-title'),
             $userInfo = $thisTweet.children('.h-card').children('.profile');
@@ -55,21 +65,33 @@ var getTweet = function () {
       }
 
       if (tweets.length > 1) {
-        hereYouGo(tweets, callbackFn);
+        hereYouGo(tweets, name);
       } else {
         var tweet = tweets[0];
-        hereYouGo(tweet, callbackFn);
+        hereYouGo(tweet, name);
       }
     }
   }
-  
-  function hereYouGo (out, callback) {
+
+  function hereYouGo (out, name) {
     if (Array.isArray(out)) {
-      getTweet.tweets = out,
-      getTweet.tweet  = out[0];
+      getTweet[name].tweets = out,
+      getTweet[name].tweet  = out[0];
     } else {
-      getTweet.tweet  = out;
+      getTweet[name].tweet  = out;
     }
-    callback();
+
+    if (name === 'gtDefault') {
+      getTweet.tweet  = getTweet.gtDefault.tweet,
+      getTweet.tweets = getTweet.gtDefault.tweets;
+      
+      getTweet.gtDefault.callback();
+      
+      getTweet.gtDefault = {},
+      delete getTweet.gtDefault;
+    } else {
+      getTweet[name].callback();
+    }
+    
   }
 }();
