7dfaac5b7e99e256f9fc5b58da96c71b07c612a4
Jimmy King <jimmy.king@meltmedia.com>
2013-09-27 23:06:47 -0700

    Fixed grunt mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,33 @@ module.exports = function(grunt) {
   grunt.initConfig({
   	pkg: grunt.file.readJSON('package.json'),
 
+    jshint: {
+      files: [
+        'Gruntfile.js',
+        'get-tweet.js',
+        'test/**/*.js',
+        'demo/**/*.js'
+      ],
+      options: {
+        ignores: ['test/lib/**/*.js'],
+        jshintrc: './.jshintrc'
+      }
+    },
+
     mocha: {
-      index: [ 'test/index.html' ]
+      test: {
+        src: ['test/index.html'],
+        options: {
+          reporter: 'List',
+          run: true
+        }
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-node-version');
 
   grunt.registerTask('test', ['mocha']);
